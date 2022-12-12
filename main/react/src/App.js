e6ffd36f6a51d3669f01ca2060f82f7361a6b695
Graham <1155163071@link.cuhk.edu.hk>
2022-12-12 15:47:01 +0800

    Removed reactdom import

--- main/react/src/App.js
+++ main/react/src/App.js
@@ -1,5 +1,3 @@
-
-import ReactDOM from 'react-dom/client';
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { useParams, useLocation } from 'react-router-dom';
