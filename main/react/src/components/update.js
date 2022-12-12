b04b1dd14e989de3ecee4267123e8bd9533913ca
Sumwai0920 <114902700+Sumwai0920@users.noreply.github.com>
2022-12-12 23:11:24 +0800

    Updated update.js

--- main/react/src/components/update.js
+++ main/react/src/components/update.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useParams, useLocation } from 'react-router-dom';
+
+
+
 
 function Update(){
+    let {chosen} = useParams();
+    //form 1
 
     const load = (event) => {
         event.preventDefault();
@@ -9,35 +15,114 @@ function Update(){
     const update = (event) => {
         event.preventDefault();
     }
-
+     if( chosen==1){ 
     return(
         <>
-        <h1> Update Event </h1>
+
+<div className="text-center">
+<h2 style={{background:"rosybrown", color:"white", textAlign:"center"}}>Update Event</h2>
+        
      
     
         <form action="" method="post">
-       
-       <label htmlFor="eventId">Event Id</label>
-       <input type="text" id="eventId" name="eventId" required/>
-       <button type="button" onClick={e => load(e)}>Load Information</button>
-       <br/>
-          
-       <label htmlFor="name">Event name</label>
-       <input type="text" id="name" name="name"/>
-       <br/>
-           
-       <label htmlFor="locId">Event location</label>
-       <input type="text" id="locId" name="locId"/>
-       <br/>
-           
-       <label htmlFor="quota">Location quota</label>
-       <input type="text" id="quota" name="quota"/>
-       <br/>
-       
-        <button type="button"   onClick={e => update(e)}>Update</button>
-       </form>
-       </>
-    );
-}
 
+        <label htmlFor="eventId"  className="d-none">Event Id</label>
+        <input type="text" className="form-control" style={{width:400, margin:'auto'}} placeholder="EventID" id="eventId" name="eventId" required/>
+    <br/>
+        
+       <button className="btn btn-lg btn-block" style={{width:400, margin:'auto', backgroundColor: 'rosybrown', color:'white'}} type="button" onClick={e => load(e)}>Load Information</button>
+       <br/> <br/> 
+
+       <label htmlFor="title" className="d-none">Title</label>
+        <input type="text" className="form-control" style={{width:400, margin:'auto'}} placeholder="Event Title" id="title" name="title"/>
+        <br/> 
+          
+       
+        <label htmlFor="venue" className="d-none">Venue</label>
+        <input type="text" className="form-control" style={{width:400, margin:'auto'}} placeholder="Event Venue" id="venue" name="venue"/>
+        <br/>
+
+        <label htmlFor="date" className="d-none">Date</label>
+        <input type="text" className="form-control" style={{width:400, margin:'auto'}} placeholder="Event Date" id="date" name="date"/>
+        <br/>
+
+        <label htmlFor="description" className="d-none">Description</label>
+        <input type="text" className="form-control" style={{width:400, margin:'auto'}} placeholder="Description" id="description" name="description"/>
+        <br/>
+
+        <label htmlFor="presenter" className="d-none">Presenter</label>
+        <input type="text" className="form-control" style={{width:400, margin:'auto'}} placeholder="Presenter" id="presenter" name="presenter"/>
+        <br/>
+
+        <label htmlFor="price" className="d-none">Price</label>
+        <input type="text" className="form-control" style={{width:400, margin:'auto'}} placeholder="Price" id="price" name="price"/>
+        <br/>
+
+       
+        <button className="btn btn-lg btn-block" style={{width:400, margin:'auto', backgroundColor: 'rosybrown', color:'white'}} type="button"   onClick={e => update(e)}>Update</button>
+       </form></div>
+       </>
+    );}
+    //form 2  
+    else if( chosen==2){ 
+        return(
+            <>
+            <div className="text-center">
+            <h2 style={{background:"rosybrown", color:"white", textAlign:"center"}}>Update Location</h2>
+
+        
+            <form action="" method="post">
+
+            <label htmlFor="locationId"  className="d-none">Location ID</label>
+            <input type="text" className="form-control" style={{width:400, margin:'auto'}} placeholder="Location ID" id="locationId" name="locationId" required/>
+            <br/>
+           
+            <button className="btn btn-lg btn-block" style={{width:400, margin:'auto', backgroundColor: 'rosybrown', color:'white'}} type="button" onClick={e => load(e)}>Load Information</button>
+       <br/> <br/> 
+            
+            <label htmlFor="name" className="d-none">Location Name</label>
+            <input type="text" className="form-control" style={{width:400, margin:'auto'}} placeholder="Location Name" id="name" name="name"/>
+            <br/>
+              
+            <label htmlFor="latitude" className="d-none">Latitude</label>
+            <input type="text" className="form-control" style={{width:400, margin:'auto'}} placeholder="Latitude" id="latitude" name="latitude"/>
+            <br/>
+
+            <label htmlFor="longitude" className="d-none">Longitude</label>
+            <input type="text" className="form-control" style={{width:400, margin:'auto'}} placeholder="Longitude" id="longitude" name="longitude"/>
+            <br/>
+                      
+            <button className="btn btn-lg btn-block" style={{width:400, margin:'auto', backgroundColor: 'rosybrown', color:'white'}} type="button"   onClick={e => update(e)}>Update</button>
+           </form></div>
+           </>
+        );}
+     //form 3  
+     else if( chosen==3){ 
+        return(
+            <>
+            <div className="text-center">
+            <h2 style={{background:"rosybrown", color:"white", textAlign:"center"}}>Update User</h2>
+
+            
+            <form action="" method="post">
+
+            <label htmlFor="name"  className="d-none">User Name</label>
+            <input type="text" className="form-control" style={{width:400, margin:'auto'}} placeholder="User Name" id="name" name="name"/>
+            <br/>
+
+           
+            <button className="btn btn-lg btn-block" style={{width:400, margin:'auto', backgroundColor: 'rosybrown', color:'white'}} type="button" onClick={e => load(e)}>Load Information</button>
+       <br/> <br/> 
+            
+         <label htmlFor="password" className="d-none">User Password</label>
+            <input type="text" className="form-control" style={{width:400, margin:'auto'}} placeholder="User Password" id="password" name="password"/>
+            <br/>
+                      
+            <button className="btn btn-lg btn-block" style={{width:400, margin:'auto', backgroundColor: 'rosybrown', color:'white'}} type="button"   onClick={e => update(e)}>Update</button>
+           </form></div>
+           </>
+        );
+
+}
+}
 export default React.memo(Update) 
