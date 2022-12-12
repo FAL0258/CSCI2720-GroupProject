0d1427972bb424ade3bf3a09578431e167e5a9f1
Graham <1155163071@link.cuhk.edu.hk>
2022-12-12 19:32:56 +0800

    Fixed map

--- main/react/src/components/map.js
+++ main/react/src/components/map.js
@@ -1,63 +1,64 @@
-import React, { useState } from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React, { useState, useCallback } from "react";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
-const locData = [{lat:22.501639,lng:114.128911 },{lat:22.391810,lng:113.976771},{lat:22.35665,lng:114.12623},{lat:22.44152,lng:114.02289 }];
+/**
+ * refs: https://www.npmjs.com/package/@react-google-maps/api
+ *       https://www.youtube.com/watch?v=9e-5QHpadi0
+ */
 
-const containerStyle = {
-    width: '800px',
-    height: '600px',
+const locdata = [
+  { lat: 22.501639, lng: 114.128911 },
+  { lat: 22.39181, lng: 113.976771 },
+  { lat: 22.35665, lng: 114.12623 },
+  { lat: 22.44152, lng: 114.02289 },
+];
+
+const Maps = (props) => {
+  const containerStyle = {
+    width: props.mapWidth,
+    height: props.mapHeight,
     featureType: "poi",
     elementType: "labels",
 
-    stylers: [{ visibility: "off" }]
+    stylers: [{ visibility: "off" }],
   };
-  
-const center = { //to see the whole HK map
+
+  const center = {
+    //to see the whole HK map
     lat: 22.3453,
-    lng: 114.1372
+    lng: 114.1372,
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyD0bhHj99UCaw3koa6z2St9AwIJYfn7xHo",
+  });
+
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback((map) =>{
+    map.setZoom(11);
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback((map) => {
+    setMap(null);
+  }, []);
+  
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {locdata.map((coordinates) => <Marker position={coordinates} />)};
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 };
-
-const Maps = () => {
-    const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: "AIzaSyD0bhHj99UCaw3koa6z2St9AwIJYfn7xHo"
-    });
-
-    const [map, setMap] = useState(null);
-
-    const onLoad = React.useCallback((map) => {
-        map.setZoom(11);
-        setMap(map);
-    }, []);
-
-    const onUnmount = React.useCallback((map) => {
-        setMap(null);
-    }, []);
-
-
-    const addMarker = (item) => {
-        let marker = new window.google.maps.Marker({
-            position: item,
-            map: map
-        });
-
-        locData.map(addMarker);
-
-        return isLoaded ? (
-        
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={center}
-                zoom={10}
-                onLoad={onLoad}
-                onUnmount={onUnmount}
-            >
-            { /* Child components, such as markers, info windows, etc. */ }
-            <></>
-            </GoogleMap>
-        
-        ) : <></>
-    }
-}
 
 export default React.memo(Maps);
