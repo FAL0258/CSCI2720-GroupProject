4f90fc1666acd40054e6228f3c8b0d2ac7ddcb94
Falcon <falfzz28@gmail.com>
2022-12-13 20:04:06 +0800

    Added credential verfitication

---
+++ main/react/src/config.js
@@ -0,0 +1,10 @@
+// Use this file to store all the environment variables
+// Add header by import GLB from "./config.js"
+// Access with GLB.var
+
+const BACKEND_IP = "54.87.45.159";
+const BACKEND_PORT =  "4000";
+
+export default {
+    BACKEND_API: ("http://" + BACKEND_IP + ":" + BACKEND_PORT)
+};
