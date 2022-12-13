2e787faf749d7ea20f9a2ad5047f42aebff54a12
Sumwai0920 <114902700+Sumwai0920@users.noreply.github.com>
2022-12-13 17:13:50 +0800

    Update crudevent.js

--- main/react/src/components/crudevent.js
+++ main/react/src/components/crudevent.js
@@ -9,22 +9,22 @@ function CRUDevent (){
            <div className="container">
       <div className="row">
       
-      <h2 className="my-5" style={{background:"#DEC7C7", color:"#5E4747", textAlign:"center", borderRadius:20}}>Choose one from the following actions:</h2>
+      <h2 className="my-3" style={{background:"#DEC7C7", color:"#5E4747", textAlign:"center", borderRadius:20}}>Choose one from the following actions:</h2>
       </div></div>
         
      <div className="container">
       <div className="row">
    
-    <div className="col-sm-12 col-md-3 my-4" style={{display:'flex',justifyContent:'center'}}>
+    <div className="col-sm-12 col-md-3 my-3" style={{display:'flex',justifyContent:'center'}}>
     <Link to={"/create/"+chosen}><button type="button" className="btn btn-lg" style={{backgroundColor: 'rosybrown', color:'white'}}>Create</button></Link>
     </div>
-    <div className="ccol-sm-12 col-md-3 my-4" style={{display:'flex',justifyContent:'center'}}>
+    <div className="ccol-sm-12 col-md-3 my-3" style={{display:'flex',justifyContent:'center'}}>
     <Link to={"/read/"+chosen}><button type="button" className="btn btn-lg" style={{backgroundColor: 'rosybrown', color:'white'}}>Read</button></Link>
     </div>
-    <div className="ccol-sm-12 col-md-3 my-4" style={{display:'flex',justifyContent:'center'}}>
+    <div className="ccol-sm-12 col-md-3 my-3" style={{display:'flex',justifyContent:'center'}}>
       <Link to={"/update/"+chosen}><button type="button" className="btn btn-lg" style={{backgroundColor: 'rosybrown', color:'white'}}>Update</button></Link>
     </div>
-    <div className="col-sm-12 col-md-3 my-4" style={{display:'flex',justifyContent:'center'}}>
+    <div className="col-sm-12 col-md-3 my-3" style={{display:'flex',justifyContent:'center'}}>
     <Link to={"/delete/"+chosen}><button type="button" className="btn btn-lg" style={{backgroundColor: 'rosybrown', color:'white'}}>Delete</button></Link>
     </div>
     </div></div>
