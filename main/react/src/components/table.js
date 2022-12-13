58b11b0fd5c5479a2f1b6c6a49e025d3fb0ef751
Graham <1155163071@link.cuhk.edu.hk>
2022-12-13 17:00:16 +0800

    Implemented sorting

--- main/react/src/components/table.js
+++ main/react/src/components/table.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import SearchBox from "./searchbox.js"
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import SearchBox from "./searchbox.js";
 
 let data1 = [
   { loc: "Tai Po Public Library", num: "4" },
@@ -21,8 +20,10 @@ const TableRow = (props) => {
   let url = "/locationpage/" + i;
   return (
     <tr>
-      <td><Link to={url}> {data1[i].loc}</Link></td>
-      
+      <td>
+        <Link to={url}> {data1[i].loc}</Link>
+      </td>
+
       <td>{data1[i].num}</td>
     </tr>
   );
@@ -31,26 +32,105 @@ const TableRow = (props) => {
 const Tables = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [filtered, setFiltered] = useState([]);
+  const [sortChoice, setSortChoice] = useState("");
 
   const handleInput = (e) => {
     setSearchQuery(e.target.value);
-  }
-  
+  };
+
+  const handleSort = (e) => {
+    setSortChoice(e.target.value);
+  };
+
   const filterData = (data) => {
     return data.loc.toLowerCase().includes(searchQuery.toLowerCase());
+  };
+
+  const sortByName = (item1, item2) => {
+    if (item1.loc < item2.loc) {
+      return -1;
+    }
+    if (item1.loc > item2.loc) {
+      return 1;
+    }
+    return 0;
+  }
+
+  const sortByNumber = (item1, item2) => {
+    if (parseInt(item1.num) < parseInt(item2.num)) {
+      return -1;
+    }
+    if (parseInt(item1.num) > parseInt(item2.num)) {
+      return 1;
+    }
+    return 0;
   }
 
   // Debugging
   useEffect(() => {
-    setFiltered(data1.filter(filterData));
-  }, [searchQuery]);
+    let results = data1.filter(filterData);
+    if (sortChoice == "name") {
+      results.sort(sortByName);
+    }
+    else if (sortChoice == "number") {
+      results.sort(sortByNumber);
+    }
+    setFiltered(results);
+  }, [searchQuery, sortChoice]);
 
   return (
     <main>
       <div className="main">
         <div className="form-group has-search">
           <span className="fa fa-search form-control-feedback"></span>
-          <input type="text" className="form-control" placeholder="Search" onChange={handleInput} value={searchQuery} />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search"
+            onChange={handleInput}
+            value={searchQuery}
+          />
+        </div>
+        <br />
+        <p>Sort by: </p>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="sortChoice"
+            id="noSort"
+            value="none"
+            onChange={handleSort}
+          />
+          <label className="form-check-label" for="noSort">
+            None
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="sortChoice"
+            id="nameSort"
+            value="name"
+            onChange={handleSort}
+          />
+          <label className="form-check-label" for="nameSort">
+            Name
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="sortChoice"
+            id="numberSort"
+            value="number"
+            onChange={handleSort}
+          />
+          <label className="form-check-label" for="numberSort">
+            Number
+          </label>
         </div>
       </div>
       <br />
@@ -68,13 +148,16 @@ const Tables = () => {
           <tbody>
             {filtered.map((d, index) => {
               let url = "/locationpage/" + index;
-              return(
+              return (
                 <tr>
-                  <td><Link to={url}> {filtered[index].loc}</Link></td>
-                  
+                  <td>
+                    <Link to={url}> {filtered[index].loc}</Link>
+                  </td>
+
                   <td>{filtered[index].num}</td>
                 </tr>
-            )})}
+              );
+            })}
           </tbody>
         </table>
       </div>
