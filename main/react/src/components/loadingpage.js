e76bbd7e5ad3e54dd8932ff129128e9da987d964
Sumwai0920 <114902700+Sumwai0920@users.noreply.github.com>
2022-12-14 01:32:55 +0800

    added loading page.js

---
+++ main/react/src/components/loadingpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LoadingPage = () => {
+    return(
+<div className="text-center" style={{marginTop:30+'vh'}}>
+<button style={{color:'black', width:40+'vw', fontSize:5+'vw',border:2}} className="btn" type="button" disabled>
+<span class="spinner-border spinner-border-sm" style={{width:1+'em', height:1+'em'}} role="status" aria-hidden="true"></span>
+   Loading...
+</button>
+</div>
+    );
+}
+
+export default React.memo(LoadingPage);
