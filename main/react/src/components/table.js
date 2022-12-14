59fdcd2384a293fe2b0bd1530377379ac493c8ae
Sumwai0920 <114902700+Sumwai0920@users.noreply.github.com>
2022-12-14 17:32:58 +0800

    delete useless data

--- main/react/src/components/table.js
+++ main/react/src/components/table.js
@@ -22,18 +22,6 @@ import { Link } from "react-router-dom";
  *    "/locationpage/123" or something and not array index, will change implementation once the
  *    backend has been set up
  */
-let data1 = [
-  { loc: "Tai Po Public Library", num: "4" },
-  { loc: "Sha Tin Town Hall", num: "6" },
-  { loc: "Fa Yuen Street Public Library", num: "9" },
-  { loc: "Fanling Public Library", num: "5" },
-  { loc: "Lek Yuen Public Library", num: "10" },
-  { loc: "Lung Hing Public Library", num: "7" },
-  { loc: "Ngau Chi Wan Public Library", num: "8" },
-  { loc: "Hong Kong Film Archive", num: "3" },
-  { loc: "North Lamma Public Library", num: "4" },
-  { loc: "Emperor Cinemas iSQUARE", num: "8" },
-];
 
 
 
