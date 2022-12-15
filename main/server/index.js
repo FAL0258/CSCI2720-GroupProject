bd209b9dccb6dff6211de023bbd15d8991c9c0a0
fymf114 <117441889+fymf114@users.noreply.github.com>
2022-12-15 18:39:18 +0800

    Fix typo @ read3

--- main/server/index.js
+++ main/server/index.js
@@ -352,7 +352,7 @@ app.post('/create/3',(req,res)=>{
     User.findOne({userId:req.params['userId']}).exec(function(err,user){
       if(err||user==null||user==undefined)
         res.status(404).set('Content-Type', 'text/plain').send("User not found!");
-      else res.set('Content-Type', 'text-plain').send(user);
+      else res.set('Content-Type', 'text/plain').send(user);
     })
   });
   //Update Event
