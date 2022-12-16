86125f28d248ebdff50519866cb98c7a438dbcaa
Falcon <falfzz28@gmail.com>
2022-12-17 04:42:41 +0800

    .

--- main/react/src/config.js
+++ main/react/src/config.js
@@ -6,5 +6,5 @@ const BACKEND_IP = "54.167.46.74";
 const BACKEND_PORT = "4000";
 
 export default {
-    BACKEND_API: ("http://" + BACKEND_IP + ":" + BACKEND_PORT)
+    BACKEND_API: "https://csci2720-gp1-culture.herokuapp.com"
 };
