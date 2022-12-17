ea5e9115e7e8561ab61d83a7758931e37c88a8f8
Falcon <falfzz28@gmail.com>
2022-12-17 14:11:06 +0800

    .

--- main/react/src/config.js
+++ main/react/src/config.js
@@ -2,9 +2,9 @@
 // Add header by import GLB from "./config.js"
 // Access with GLB.BACKEND_API => something like "http://127.0.0.1:4000"
 
-const BACKEND_IP = "54.167.46.74";
+const BACKEND_IP = "18.232.182.130";
 const BACKEND_PORT = "4000";
 
 export default {
-    BACKEND_API: "https://csci2720-gp1-culture.herokuapp.com"
+    BACKEND_API: ("http://" + BACKEND_IP + ":" + BACKEND_PORT)
 };
